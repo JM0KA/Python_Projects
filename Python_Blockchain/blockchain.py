086cafbda81f44bb321423f86fbed78d95627d92
JM0KA <117128993+JM0KA@users.noreply.github.com>
2025-01-11 22:18:05 -0700

    added verify_transactions() method
    This method is different from the verify_transaction(transaction) method as it uses the latter to verify all transaction in the open_transactions using a list comprehension

--- Python_Blockchain/blockchain.py
+++ Python_Blockchain/blockchain.py
@@ -16,7 +16,6 @@ def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
 
 
-
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transaction if tx['sender'] == participant]
@@ -119,6 +118,11 @@ def verify_chain():
             return False
     return True
 
+
+def verify_transactions():
+    """ Verify that all the transactions within the chain are valid """
+    return all([verify_transaction(tx) for tx in open_transaction])
+
 waiting_for_input = True
 
 while waiting_for_input:
@@ -127,6 +131,7 @@ while waiting_for_input:
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
     print('4: Output participants')
+    print('5: Check transaction validity')
     print('h: Manipulate the chain')
     print('q: Quit')
     
@@ -148,6 +153,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
